2c54982ae15d8412fa215d3decb5005bb47215d0
Pedro Matiello <pmatiello@gmail.com>
2009-08-17 14:09:16 -0300

    Template changed to allow latex to build code inside note boxes.

--- src/main/java/br/com/caelum/tubaina/parser/latex/NoteTag.java
+++ src/main/java/br/com/caelum/tubaina/parser/latex/NoteTag.java
@@ -7,6 +7,9 @@ public class NoteTag implements Tag {
 	public String parse(String string, String title) {
 		// The first \n is the title/content sepparator
 		
-		return "\\boxtag{" + ((title == null)?"":title)  + "}{"+ 	string + "}";
+		return "\\boxtag{" + ((title == null)?"":title)  + "}" +
+				"{"+ 
+				string + 
+				"}";
 	}
 }
