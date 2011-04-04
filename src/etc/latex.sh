18c0ebc4f226599a2b66c7760f9e34582c6dd572
Ceci Fernandes <ceci@vidageek.net>
2011-04-04 18:22:00 -0300

    updating generation script to workaround missing answer.tex

--- src/etc/latex.sh
+++ src/etc/latex.sh
@@ -1,6 +1,9 @@
 #! /bin/sh
 echo Gerando latex...
 
-yes s | pdflatex $1.tex > /dev/null
-makeindex $1.idx > /dev/null
-yes s | pdflatex $1.tex > /dev/null
+HAS_ANSWER=`ls | grep answer.tex -c`
+if [ "$HAS_ANSWER" -eq "0" ]; then
+    touch answer.tex;
+fi
+echo "s\n" | pdflatex -shell-escape $1.tex > /dev/null
+echo "s\n" | pdflatex -shell-escape $1.tex > /dev/null
