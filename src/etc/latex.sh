9c446b3ed45cbdcc625c9eb00e401a9457f20601
Ceci Fernandes <ceci@vidageek.net>
2011-04-19 19:56:05 -0300

    playing with encoding in order to make special characters work inside code

--- src/etc/latex.sh
+++ src/etc/latex.sh
@@ -5,5 +5,9 @@ HAS_ANSWER=`ls | grep answer.tex -c`
 if [ "$HAS_ANSWER" -eq "0" ]; then
     touch answer.tex;
 fi
+
+mv book.tex book-utf.tex
+iconv --from-code=UTF-8 --to-code=ISO_8859-1 book-utf.tex > book.tex
+
 echo "s\n" | pdflatex -shell-escape $1.tex > /dev/null
 echo "s\n" | pdflatex -shell-escape $1.tex > /dev/null
