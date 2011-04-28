05285f77390d587ffd0826494b5c10670152b0af
Ceci Fernandes <ceci@vidageek.net>
2011-04-28 16:46:14 -0300

    changing script to use filename and extension

--- src/etc/latex.sh
+++ src/etc/latex.sh
@@ -1,13 +1,13 @@
 #! /bin/sh
 echo Gerando latex...
 
-HAS_ANSWER=`ls | grep answer.tex -c`
+HAS_ANSWER=`ls "$(dirname $1)" | grep answer.tex -c`
 if [ "$HAS_ANSWER" -eq "0" ]; then
-    touch answer.tex;
+    touch "$(dirname $1)"/answer.tex;
 fi
 
-mv book.tex book-utf.tex
-iconv --from-code=UTF-8 --to-code=ISO_8859-1 book-utf.tex > book.tex
+mv "$1" book-utf.tex
+iconv --from-code=UTF-8 --to-code=ISO_8859-1 book-utf.tex > "$1"
 
-echo "s\n" | pdflatex -shell-escape $1.tex > /dev/null
-echo "s\n" | pdflatex -shell-escape $1.tex > /dev/null
+echo "s\n" | pdflatex -shell-escape "$1" > /dev/null
+echo "s\n" | pdflatex -shell-escape "$1" > /dev/null
