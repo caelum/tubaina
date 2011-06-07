28c4dca8a4e2a2df79a43ca73f6b34182b904785
Ceci Fernandes <ceci@vidageek.net>
2011-06-07 17:43:29 -0300

    making html code tag stop indenting and generating pre with proper class and data-highlight atributes

--- src/main/java/br/com/caelum/tubaina/parser/html/CodeTag.java
+++ src/main/java/br/com/caelum/tubaina/parser/html/CodeTag.java
@@ -1,75 +1,52 @@
 package br.com.caelum.tubaina.parser.html;
 
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import br.com.caelum.tubaina.parser.CodeHighlightTag;
 import br.com.caelum.tubaina.parser.Indentator;
 import br.com.caelum.tubaina.parser.Tag;
 
 public class CodeTag implements Tag {
 
-	public static final String BEGIN = "<div class=\"java\"><code class=\"java\">";
-	public static final String END = "</code></div>";
+	public static final String START_HEADER = "<pre";
+	public static final String CLOSE_HEADER = ">\n";
+	public static final String END = "\n</pre>";
 
 	private final Indentator indentator;
 
 	public CodeTag(Indentator indentator) {
 		this.indentator = indentator;
 	}
-	public String parse(String string, String options) {
-		
-		boolean properties = isProperties(options);
-		List<Integer> highlights = new CodeHighlightTag().getHighlights(options);
-		
-		//indenting
-		string = indentator.indent(string);
-		
-		string = parseSpaces(string);
-		if (properties) {
-			string = parseValue(string);
-			string = parseComments(string);	
-		}
-		string = new CodeHighlightTag().parseHtml(string, highlights);
-		return BEGIN + string + END;
-	}
-	
-	private boolean isProperties(String options) {
-		return options != null && options.contains("properties");
-	}
-	
-	private String parseSpaces(String string) {
-		string = string.replaceAll("\t", "&nbsp;&nbsp;&nbsp;&nbsp;");
-		string = string.replaceAll(" ", "&nbsp;");
-		string = string.replaceAll("\n", "<br />\n");
-		return string;
-	}
-	
-	private String parseComments(String string) {
-		return matchPattern(string, "(?i)(^|[^\\\\])(#)", "texcomment");
-	}
-	private String parseValue(String string) {
-		return matchPattern(string, "(?i)(^[^#]*?[^\\\\#]&nbsp;|=|:)(.+)", "texvalue");
-	}
-	private String matchPattern(String string, String regex, String tag) {
-		Pattern pattern = Pattern.compile(regex);
-		
-		String[] lines = string.split("\n");
-		String output = "";
-		for (String line : lines) {
-			Matcher matcher = pattern.matcher(line);
-			if (matcher.find()) {
-				output += line.substring(0, matcher.start(2));
-				output += "<span class=\"" + tag + "\">";
-				output += line.substring(matcher.start(2));
-				output += "</span>\n";
-			} else {
-				output += line + "\n";
-			}
-		}
-		output = output.substring(0, Math.max(output.length() - 1,0));
-		return output;
-	}
+	public String parse(String content, String options) {
+		String preOptions = classAssembler(options);
 
+		return START_HEADER + preOptions + CLOSE_HEADER + content + END;
+	}
+	
+	private String classAssembler(String options) {
+		String language = detectLanguage(options);
+		String numbered = options.contains("#") ? " numbered" : "";
+		String highlights = detectHighlights(options);
+
+		return " class=\"code" + language + numbered + "\"" + highlights;
+	}
+	
+	private String detectLanguage(String options) {
+		if (options != null) {
+			String languageCandidate = options.split(" ")[0];
+			if (!languageCandidate.contains("#") && !languageCandidate.startsWith("h=") && !languageCandidate.isEmpty())
+				return " " + languageCandidate;
+		}
+		return " text";
+	}
+	
+	private String detectHighlights(String options) {
+		Pattern pattern = Pattern.compile("h=([\\d+,]+)");
+		Matcher matcher = pattern.matcher(options);
+		if (matcher.find()) {
+			return " data-highlight=\"" + matcher.group(1) + "\"";
+		}
+		return "";
+	}
+	
 }
--- src/test/java/br/com/caelum/tubaina/parser/html/CodeTagTest.java
+++ src/test/java/br/com/caelum/tubaina/parser/html/CodeTagTest.java
@@ -2,22 +2,79 @@ package br.com.caelum.tubaina.parser.html;
 
 import junit.framework.Assert;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import br.com.caelum.tubaina.parser.SimpleIndentator;
 
 
 public class CodeTagTest {
+	
+	private String code;
+
+	@Before
+	public void setUp() {
+		code =  "public static void main(String[] args) {\n" +
+				"	String name = \"Gabriel\";\n" +
+				"	System.out.println(\"Hello, \" + name);\n" +
+				"}";
+	}
+	
+	@Test
+	public void plainJavaCode() throws Exception {
+		String options = "java";
+		String output = new CodeTag(new SimpleIndentator()).parse(code, options);
+		Assert.assertEquals("<pre class=\"code java\">\n" + code + "\n</pre>", output);
+	}
 
 	@Test
-	public void testCodeWithHighlights() throws Exception {
-		String options = "h=1,3,5\n";
-		String code = "Algum texto\n" +
-				"Em várias linhas\n" +
-				"Mas não necessariamente 5";
+	public void javaCodeWithHighlights() throws Exception {
+		String options = "java h=1,4\n";
 		String output = new CodeTag(new SimpleIndentator()).parse(code, options);
-		Assert.assertEquals(CodeTag.BEGIN + "<strong>Algum&nbsp;texto<br /></strong>\n" +
-				"Em&nbsp;várias&nbsp;linhas<br />\n" +
-				"<strong>Mas&nbsp;não&nbsp;necessariamente&nbsp;5</strong>" + CodeTag.END, output);
+		Assert.assertEquals("<pre class=\"code java\" data-highlight=\"1,4\">\n" + code + "\n</pre>", output);
+	}
+	
+	@Test
+	public void javaCodeWithNumberedLines() throws Exception {
+		String options="java #";
+		String output = new CodeTag(new SimpleIndentator()).parse(code, options);
+		Assert.assertEquals("<pre class=\"code java numbered\">\n" + code + "\n</pre>", output);
+	}
+	
+	@Test
+	public void plainRubyCode() throws Exception {
+		String options = "ruby";
+		String rubyCode = "@name = \"Gabriel\"\n" +
+						  "puts \"Hello, \" + name";
+		String output = new CodeTag(new SimpleIndentator()).parse(rubyCode, options);
+		Assert.assertEquals("<pre class=\"code ruby\">\n" + rubyCode + "\n</pre>", output);
+	}
+	
+	@Test
+	public void noLanguageDefinedIsTreatedAsText() throws Exception {
+		String options="";
+		String noParticularLanguage = "Some text explaining some new bizarre\n" +
+										"syntax in a very code alike way";
+		String output = new CodeTag(new SimpleIndentator()).parse(noParticularLanguage, options);
+		Assert.assertEquals("<pre class=\"code text\">\n" 
+													+ noParticularLanguage + "\n</pre>", output);
+	}
+	@Test
+	public void noLanguageDefinedIsTreatedAsTextEvenWhenItIsNumbered() throws Exception {
+		String options="#";
+		String noParticularLanguage = "Some text explaining some new bizarre\n" +
+										"syntax in a very code alike way";
+		String output = new CodeTag(new SimpleIndentator()).parse(noParticularLanguage, options);
+		Assert.assertEquals("<pre class=\"code text numbered\">\n" 
+				+ noParticularLanguage + "\n</pre>", output);
+	}
+	@Test
+	public void noLanguageDefinedIsTreatedAsTextEvenWhenItHasHighlights() throws Exception {
+		String options="h=2";
+		String noParticularLanguage = "Some text explaining some new bizarre\n" +
+										"syntax in a very code alike way";
+		String output = new CodeTag(new SimpleIndentator()).parse(noParticularLanguage, options);
+		Assert.assertEquals("<pre class=\"code text\" data-highlight=\"2\">\n" 
+				+ noParticularLanguage + "\n</pre>", output);
 	}
 }
