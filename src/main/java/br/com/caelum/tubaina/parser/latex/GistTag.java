d24e594fa420bddd17f1315582ef63420fc54bf2
Francisco Sokol <chico.sokol@gmail.com>
2012-02-29 20:04:34 -0300

    adding line number support to gist tag

--- src/main/java/br/com/caelum/tubaina/parser/latex/GistTag.java
+++ src/main/java/br/com/caelum/tubaina/parser/latex/GistTag.java
@@ -7,21 +7,25 @@ import br.com.caelum.tubaina.parser.Tag;
 
 public class GistTag implements Tag {
 
-	private CodeTag code;
-	private GistResultRetriever retriever;
+    private CodeTag code;
+    private GistResultRetriever retriever;
 
-	public GistTag(Indentator i, GistResultRetriever retriever) {
-		this.code = new CodeTag(i);
-		this.retriever = retriever;
-	}
+    public GistTag(Indentator i, GistResultRetriever retriever) {
+        this.code = new CodeTag(i);
+        this.retriever = retriever;
+    }
 
-	public String parse(String string, String options) {
-		long gistId = Long.parseLong(options);
-		GistResult result = retriever.retrieve(gistId, "");
-		
-		String content = result.getContent();
-		String language = result.getLanguage();
-		
-		return code.parse(content, language);
-	}
+    public String parse(String string, String options) {
+        String codeOptions = options.contains("#") ? " #" : "";
+        
+        long gistId = Long.parseLong(options.replaceAll("\\D", ""));
+        
+        GistResult result = retriever.retrieve(gistId, "");
+        
+        String content = result.getContent();
+        String language = result.getLanguage();
+        
+        return code.parse(content, language + codeOptions);
+    }
+    
 }
--- src/test/java/br/com/caelum/tubaina/parser/latex/GistTagTest.java
+++ src/test/java/br/com/caelum/tubaina/parser/latex/GistTagTest.java
@@ -1,10 +1,16 @@
 package br.com.caelum.tubaina.parser.latex;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.Scanner;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 import br.com.caelum.tubaina.gists.GistConnector;
 import br.com.caelum.tubaina.gists.GistRequest;
+import br.com.caelum.tubaina.gists.GistResult;
 import br.com.caelum.tubaina.gists.GistResultRetriever;
 import br.com.caelum.tubaina.gists.JsonToGistResultConverter;
 import br.com.caelum.tubaina.parser.SimpleIndentator;
@@ -13,14 +19,42 @@ public class GistTagTest {
 
 	@Test
 	public void gistedCodeIsRetrievedAndUsed() throws Exception {
-		GistResultRetriever retriever = new GistResultRetriever(new GistConnector(
-				new JsonToGistResultConverter(), new GistRequest()));
+		GistResultRetriever retriever = new GistResultRetriever(
+				new GistConnector(new JsonToGistResultConverter(),
+						new GistRequest()));
 
 		String options = "417835";
 		String gistedCode = "javascript:(function() {window.frames[3][0].document.getElementById('frameplugin').style.display='none'})()";
 
-		String output = new GistTag(new SimpleIndentator(), retriever).parse("", options);
+		String output = new GistTag(new SimpleIndentator(), retriever).parse(
+				"", options);
 
-		Assert.assertEquals(CodeTag.BEGIN + "{text}\n" + gistedCode + CodeTag.END, output);
+		Assert.assertEquals(CodeTag.BEGIN + "{text}\n" + gistedCode
+				+ CodeTag.END, output);
 	}
+
+	@Test
+	public void gistedCodeIsParsedWithLineNumbers() throws Exception {
+		String gistedCode = "GivenCode";
+		String options = "417835 #";
+		long gistId = 417835;
+
+		String json = new Scanner(
+				JsonToGistResultConverter.class
+						.getResourceAsStream("/gist.json")).useDelimiter("\\Z")
+				.next();
+
+		GistRequest mockedGistRequest = mock(GistRequest.class);
+		when(mockedGistRequest.get(gistId)).thenReturn(json);
+
+		GistConnector connector = new GistConnector(new JsonToGistResultConverter(), mockedGistRequest);
+
+		GistResultRetriever retriever = new GistResultRetriever(connector);
+
+		String output = new GistTag(new SimpleIndentator(), retriever).parse(
+				"", options);
+		Assert.assertEquals(CodeTag.BEGIN + "[linenos, numbersep=5pt]{text}\n"
+				+ gistedCode + CodeTag.END, output);
+	}
+
 }
