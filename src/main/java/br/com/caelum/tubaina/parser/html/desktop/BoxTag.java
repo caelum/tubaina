11421eaa006a05b6d06e9257bf50460ec0177429
Leonardo Wolter <leocwolter@gmail.com>
2012-05-08 11:03:47 -0300

    Changing box tag for kindles

--- src/main/java/br/com/caelum/tubaina/parser/html/desktop/BoxTag.java
+++ src/main/java/br/com/caelum/tubaina/parser/html/desktop/BoxTag.java
@@ -1,14 +1,13 @@
 package br.com.caelum.tubaina.parser.html.desktop;
 
 import br.com.caelum.tubaina.parser.Tag;
-import br.com.caelum.tubaina.parser.html.BoxTagTemplate;
 
 public class BoxTag implements Tag {
 
-	private BoxTagTemplate template = new BoxTagTemplate();
-	
 	public String parse(String content, String title) {
-		return template.parse(content, title);
+
+		return "<div class=\"box\"><h4>" + title.trim() 
+				+ "</h4>\n" + content.trim() + "</div>";
 	}
 
 }
--- src/main/java/br/com/caelum/tubaina/parser/html/kindle/BoxTag.java
+++ src/main/java/br/com/caelum/tubaina/parser/html/kindle/BoxTag.java
@@ -1,14 +1,12 @@
 package br.com.caelum.tubaina.parser.html.kindle;
 
 import br.com.caelum.tubaina.parser.Tag;
-import br.com.caelum.tubaina.parser.html.BoxTagTemplate;
 
 public class BoxTag implements Tag {
 
-	private BoxTagTemplate template = new BoxTagTemplate();
-	
 	public String parse(String content, String title) {
-		return template.parse(content, title);
+		return "<hr/><h4>" + title.trim() 
+				+ "</h4>\n" + content.trim() + "<hr/>";
 	}
 
 }
