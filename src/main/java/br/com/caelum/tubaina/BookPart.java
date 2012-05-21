882af051e0436a4465c6aa4799fb1a3d146fac97
Francisco Sokol <chico.sokol@gmail.com>
2012-05-21 15:18:00 -0300

    using IntroductionChunk when parsing part introduction

--- src/main/java/br/com/caelum/tubaina/BookPart.java
+++ src/main/java/br/com/caelum/tubaina/BookPart.java
@@ -3,6 +3,11 @@ package br.com.caelum.tubaina;
 import java.util.ArrayList;
 import java.util.List;
 
+import br.com.caelum.tubaina.builder.ChunkSplitter;
+import br.com.caelum.tubaina.chunk.IntroductionChunk;
+import br.com.caelum.tubaina.parser.Parser;
+import br.com.caelum.tubaina.resources.Resource;
+
 public class BookPart {
     
     private List<Chapter> chapters;
@@ -33,6 +38,12 @@ public class BookPart {
         return printable;
     }
 
+    public String getIntroduction(Parser p) {
+        List<Resource> resources = new ArrayList<Resource>();
+        IntroductionChunk intro = new IntroductionChunk(new ChunkSplitter(resources, "all").splitChunks(introduction));
+        return intro.getContent(p);
+    }
+    
     public String getIntroduction() {
         return this.introduction;
     }
