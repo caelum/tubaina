06dd1604402452d21d2afb316246d379b65ec587
Adriano Almeida <adrianoalmeida7@gmail.com>
2012-06-14 19:17:10 -0300

    ignoring bibliography property if it is null

--- src/main/java/br/com/caelum/bibliography/LatexBibliographyGenerator.java
+++ src/main/java/br/com/caelum/bibliography/LatexBibliographyGenerator.java
@@ -2,37 +2,38 @@ package br.com.caelum.bibliography;
 
 public class LatexBibliographyGenerator implements BibliographyGenerator {
 
-    public String generateTextOf(Bibliography bibliography) {
-        StringBuffer result = new StringBuffer();
+	public String generateTextOf(Bibliography bibliography) {
+		StringBuffer result = new StringBuffer();
 
-        escapeInvalidChars(bibliography);
-        
-        for (BibliographyEntry entry : bibliography.getEntries()) {
-            result.append("@" + entry.type + "{");
-            result.append(entry.label + ",\n");
-            addField(result, "author", entry.author);
-            addField(result, "title", entry.title);
-            addField(result, "year", entry.year);
-            addField(result, "howPublished", "\\url{" + entry.howPublished + "}");
-            if (entry.publisher != null)
-                addField(result, "publisher", entry.publisher);
-            result.append("}\n");
-        }
+		escapeInvalidChars(bibliography);
 
-        return result.toString();
-    }
+		for (BibliographyEntry entry : bibliography.getEntries()) {
+			result.append("@" + entry.type + "{");
+			result.append(entry.label + ",\n");
+			addField(result, "author", entry.author);
+			addField(result, "title", entry.title);
+			addField(result, "year", entry.year);
+			addField(result, "howPublished", "\\url{" + entry.howPublished + "}");
+			if (entry.publisher != null)
+				addField(result, "publisher", entry.publisher);
+			result.append("}\n");
+		}
 
-    private void escapeInvalidChars(Bibliography bibliography) {
-        for (BibliographyEntry entry : bibliography.getEntries()) {
-            if (entry.howPublished != null)
-                entry.howPublished = entry.howPublished.replaceAll("\\_", "\\\\_");
-            if (entry.title != null)
-                entry.title = entry.title.replaceAll("\\_", "\\\\_");
-        }
-    }
+		return result.toString();
+	}
 
-    private void addField(StringBuffer result, String key, String value) {
-        result.append(key + " = {" + value + "},\n");
+	private void escapeInvalidChars(Bibliography bibliography) {
+		for (BibliographyEntry entry : bibliography.getEntries()) {
+			if (entry.howPublished != null)
+				entry.howPublished = entry.howPublished.replaceAll("\\_", "\\\\_");
+			if (entry.title != null)
+				entry.title = entry.title.replaceAll("\\_", "\\\\_");
+		}
+	}
 
-    }
+	private void addField(StringBuffer result, String key, String value) {
+		if (value != null)
+			result.append(key + " = {" + value + "},\n");
+
+	}
 }
