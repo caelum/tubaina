4fe26de5d805c996e5db04e12558aef303448d22
Francisco Sokol <chico.sokol@gmail.com>
2012-06-22 17:57:15 -0300

    cleaning test

--- src/test/java/br/com/caelum/tubaina/parser/html/desktop/SingleHtmlTOCGeneratorTest.java
+++ src/test/java/br/com/caelum/tubaina/parser/html/desktop/SingleHtmlTOCGeneratorTest.java
@@ -3,13 +3,16 @@ package br.com.caelum.tubaina.parser.html.desktop;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import br.com.caelum.tubaina.Book;
 import br.com.caelum.tubaina.TubainaBuilder;
 import br.com.caelum.tubaina.builder.BookBuilder;
 import freemarker.ext.beans.BeansWrapper;
@@ -17,31 +20,40 @@ import freemarker.template.Configuration;
 
 public class SingleHtmlTOCGeneratorTest {
     
+    private Configuration cfg;
+
+    @Before
+    public void setUp() throws IOException {
+        cfg = new Configuration();
+        cfg.setDirectoryForTemplateLoading(new File(TubainaBuilder.DEFAULT_TEMPLATE_DIR,
+                "singlehtml/"));
+        cfg.setObjectWrapper(new BeansWrapper());
+    }
+
     @Test
     public void shouldCreateATOCFromBook() throws Exception {
-        Configuration cfg = new Configuration();
-        cfg.setDirectoryForTemplateLoading(new File(TubainaBuilder.DEFAULT_TEMPLATE_DIR, "singlehtml/"));
-        cfg.setObjectWrapper(new BeansWrapper());
-        BookBuilder builder = new BookBuilder("title");
-
-        builder.addAllReaders(Arrays.asList((Reader) new StringReader(
-                "[chapter     O que é java?   ]\n" + "texto da seção\n"
-                        + "[section Primeira seção]\n" + "texto da prim seção\n"
-                        + "[section Segunda seção]\n" + "texto da segunda seção\n\n")), new ArrayList<Reader>());
-
-        builder.addAllReaders(Arrays.asList((Reader) new StringReader("[chapter Introdução]\n"
-                + "Algum texto de introdução\n")), new ArrayList<Reader>());
+        setUp();
+        Book build = createBook();
         
-        SingleHtmlTOCGenerator tocGenerator = new SingleHtmlTOCGenerator(builder.build(), cfg);
-        
+        SingleHtmlTOCGenerator tocGenerator = new SingleHtmlTOCGenerator(build, cfg);
         String toc = tocGenerator.generateTOC().toString();
-        
-        System.out.println(toc);
         
         assertTrue(toc.contains("O que é java"));
         assertTrue(toc.contains("Primeira seção"));
         assertTrue(toc.contains("Segunda seção"));
-        
+    }
+
+
+    private Book createBook() {
+        BookBuilder builder = new BookBuilder("title");
+        builder.addAllReaders(Arrays.asList((Reader) new StringReader(
+                "[chapter     O que é java?   ]\n" + "texto da seção\n"
+                        + "[section Primeira seção]\n" + "texto da prim seção\n"
+                        + "[section Segunda seção]\n" + "texto da segunda seção\n\n")), new ArrayList<Reader>());
+        builder.addAllReaders(Arrays.asList((Reader) new StringReader("[chapter Introdução]\n"
+                + "Algum texto de introdução\n")), new ArrayList<Reader>());
+        Book build = builder.build();
+        return build;
     }
 
 }
