e8cc26d47c7ff9cde5ad19fb71d7fcc25f7f14c9
Francisco Sokol <chico.sokol@gmail.com>
2012-06-25 14:50:11 -0300

    using pygments to syntax highlight code tag

--- src/main/java/br/com/caelum/tubaina/parser/html/desktop/CodeTag.java
+++ src/main/java/br/com/caelum/tubaina/parser/html/desktop/CodeTag.java
@@ -1,52 +1,66 @@
 package br.com.caelum.tubaina.parser.html.desktop;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import br.com.caelum.tubaina.TubainaException;
 import br.com.caelum.tubaina.parser.Tag;
+import br.com.caelum.tubaina.util.CommandExecutor;
 
 public class CodeTag implements Tag {
 
-	public static final String START_HEADER = "<pre";
-	public static final String CLOSE_HEADER = ">\n";
-	public static final String END = "\n</pre>";
+	public static final String START = "<div>";
+	public static final String END = "\n</div>";
+    private HtmlCodeHighlighter htmlCodeHighlighter;
+    
+    public CodeTag() {
+        this.htmlCodeHighlighter = new HtmlCodeHighlighter(new CommandExecutor());
+    }
+    
+    public CodeTag(HtmlCodeHighlighter htmlCodeHighlighter) {
+        this.htmlCodeHighlighter = htmlCodeHighlighter;
+    }
 
 	public String parse(String content, String options) {
+	    String language = detectLanguage(options);
+	    List<Integer> highlights = detectHighlights(options);
+	    boolean numbered = options.contains("#");
 	    Matcher labelMatcher = Pattern.compile("label=(\\S+)").matcher(options);
+	    
+	    if (!highlights.isEmpty()) {
+	        throw new TubainaException("Code highlights are not supported for html output yet");
+	    }
 	    if (labelMatcher.matches()) {
 	        throw new TubainaException("Code labels are not supported for html output yet");
 	    }
 	    
-		String preOptions = classAssembler(options);
-
-		return START_HEADER + preOptions + CLOSE_HEADER + content + END;
-	}
-	
-	private String classAssembler(String options) {
-		String language = detectLanguage(options);
-		String numbered = options.contains("#") ? " numbered" : "";
-		String highlights = detectHighlights(options);
-
-		return " class=\"code" + language + numbered + "\"" + highlights;
+	    String code = htmlCodeHighlighter.highlight(content, language, numbered);
+	    
+		return START + code + END;
 	}
 	
 	private String detectLanguage(String options) {
 		if (options != null) {
 			String languageCandidate = options.trim().split(" ")[0];
 			if (!languageCandidate.contains("#") && !languageCandidate.startsWith("h=") && !languageCandidate.isEmpty())
-				return " " + languageCandidate;
+				return languageCandidate;
 		}
-		return " text";
+		return "text";
 	}
 	
-	private String detectHighlights(String options) {
+	private List<Integer> detectHighlights(String options) {
+	    ArrayList<Integer> lines = new ArrayList<Integer>();
 		Pattern pattern = Pattern.compile("h=([\\d+,]+)");
 		Matcher matcher = pattern.matcher(options);
 		if (matcher.find()) {
-			return " data-highlight=\"" + matcher.group(1) + "\"";
+			String[] strings = matcher.group(1).split(",");
+			for (String string : strings) {
+                lines.add(Integer.parseInt(string));
+            }
 		}
-		return "";
+		return lines;
 	}
 	
 }
--- src/test/java/br/com/caelum/tubaina/parser/html/desktop/CodeTagTest.java
+++ src/test/java/br/com/caelum/tubaina/parser/html/desktop/CodeTagTest.java
@@ -1,5 +1,8 @@
 package br.com.caelum.tubaina.parser.html.desktop;
 
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import junit.framework.Assert;
 
 import org.junit.Before;
@@ -7,81 +10,79 @@ import org.junit.Test;
 
 import br.com.caelum.tubaina.TubainaException;
 
-
 public class CodeTagTest {
-	
-	private String code;
 
-	@Before
-	public void setUp() {
-		code =  "public static void main(String[] args) {\n" +
-				"	String name = \"Gabriel\";\n" +
-				"	System.out.println(\"Hello, \" + name);\n" +
-				"}";
-	}
-	
-	@Test
-	public void plainJavaCode() throws Exception {
-		String options = "java";
-		String output = new CodeTag().parse(code, options);
-		Assert.assertEquals("<pre class=\"code java\">\n" + code + "\n</pre>", output);
-	}
+    private String code;
 
-	@Test
-	public void javaCodeWithHighlights() throws Exception {
-		String options = "java h=1,4\n";
-		String output = new CodeTag().parse(code, options);
-		Assert.assertEquals("<pre class=\"code java\" data-highlight=\"1,4\">\n" + code + "\n</pre>", output);
-	}
-	
-	@Test
-	public void javaCodeWithNumberedLines() throws Exception {
-		String options="java #";
-		String output = new CodeTag().parse(code, options);
-		Assert.assertEquals("<pre class=\"code java numbered\">\n" + code + "\n</pre>", output);
-	}
-	
-	@Test
-	public void plainRubyCode() throws Exception {
-		String options = "ruby";
-		String rubyCode = "@name = \"Gabriel\"\n" +
-						  "puts \"Hello, \" + name";
-		String output = new CodeTag().parse(rubyCode, options);
-		Assert.assertEquals("<pre class=\"code ruby\">\n" + rubyCode + "\n</pre>", output);
-	}
-	
-	@Test
-	public void noLanguageDefinedIsTreatedAsText() throws Exception {
-		String options="";
-		String noParticularLanguage = "Some text explaining some new bizarre\n" +
-										"syntax in a very code alike way";
-		String output = new CodeTag().parse(noParticularLanguage, options);
-		Assert.assertEquals("<pre class=\"code text\">\n" 
-													+ noParticularLanguage + "\n</pre>", output);
-	}
-	@Test
-	public void noLanguageDefinedIsTreatedAsTextEvenWhenItIsNumbered() throws Exception {
-		String options="#";
-		String noParticularLanguage = "Some text explaining some new bizarre\n" +
-										"syntax in a very code alike way";
-		String output = new CodeTag().parse(noParticularLanguage, options);
-		Assert.assertEquals("<pre class=\"code text numbered\">\n" 
-				+ noParticularLanguage + "\n</pre>", output);
-	}
-	@Test
-	public void noLanguageDefinedIsTreatedAsTextEvenWhenItHasHighlights() throws Exception {
-		String options="h=2";
-		String noParticularLanguage = "Some text explaining some new bizarre\n" +
-										"syntax in a very code alike way";
-		String output = new CodeTag().parse(noParticularLanguage, options);
-		Assert.assertEquals("<pre class=\"code text\" data-highlight=\"2\">\n" 
-				+ noParticularLanguage + "\n</pre>", output);
-	}
-	
-	@Test
+    @Before
+    public void setUp() {
+        code = "public static void main(String[] args) {\n" + "	String name = \"Gabriel\";\n"
+                + "	System.out.println(\"Hello, \" + name);\n" + "}";
+    }
+
+    @Test
+    public void plainJavaCode() throws Exception {
+        String options = "java";
+        HtmlCodeHighlighter htmlCodeHighlighter = mock(HtmlCodeHighlighter.class);
+        CodeTag codeTag = new CodeTag(htmlCodeHighlighter);
+        codeTag.parse(code, options);
+        verify(htmlCodeHighlighter).highlight(eq(code), eq(options), eq(false));
+    }
+
+    @Test
+    public void javaCodeWithNumberedLines() throws Exception {
+        String options = "java #";
+        HtmlCodeHighlighter htmlCodeHighlighter = mock(HtmlCodeHighlighter.class);
+        CodeTag codeTag = new CodeTag(htmlCodeHighlighter);
+        codeTag.parse(code, options);
+        verify(htmlCodeHighlighter).highlight(eq(code), eq("java"), eq(true));
+    }
+
+    @Test
+    public void plainRubyCode() throws Exception {
+        String options = "ruby";
+        String rubyCode = "@name = \"Gabriel\"\n" + "puts \"Hello, \" + name";
+        HtmlCodeHighlighter htmlCodeHighlighter = mock(HtmlCodeHighlighter.class);
+        CodeTag codeTag = new CodeTag(htmlCodeHighlighter);
+        codeTag.parse(rubyCode, options);
+        verify(htmlCodeHighlighter).highlight(eq(rubyCode), eq("ruby"), eq(false));
+    }
+
+    @Test
+    public void noLanguageDefinedIsTreatedAsText() throws Exception {
+        String options = "";
+        String noParticularLanguage = "Some text explaining some new bizarre\n"
+                + "syntax in a very code alike way";
+        HtmlCodeHighlighter htmlCodeHighlighter = mock(HtmlCodeHighlighter.class);
+        CodeTag codeTag = new CodeTag(htmlCodeHighlighter);
+        String output = codeTag.parse(noParticularLanguage, options);
+        verify(htmlCodeHighlighter).highlight(eq(noParticularLanguage), eq("text"), eq(false));
+    }
+
+    @Test
+    public void noLanguageDefinedIsTreatedAsTextEvenWhenItIsNumbered() throws Exception {
+        String options = "#";
+        String noParticularLanguage = "Some text explaining some new bizarre\n"
+                + "syntax in a very code alike way";
+        HtmlCodeHighlighter htmlCodeHighlighter = mock(HtmlCodeHighlighter.class);
+        CodeTag codeTag = new CodeTag(htmlCodeHighlighter);
+        String output = codeTag.parse(noParticularLanguage, options);
+        verify(htmlCodeHighlighter).highlight(eq(noParticularLanguage), eq("text"), eq(true));
+    }
+
+    @Test
     public void shouldThrowExpectionWhenTagContainsLabel() {
         try {
             new CodeTag().parse("", "label=somelabel");
+            Assert.fail("should throw excpetion");
+        } catch (TubainaException e) {
+        }
+    }
+    
+    @Test
+    public void shouldThrowExpectionWhenOptionsContainsHighlight() {
+        try {
+            new CodeTag().parse("", "h=1");
             Assert.fail("should throw excpetion");
         } catch (TubainaException e) {
         }
