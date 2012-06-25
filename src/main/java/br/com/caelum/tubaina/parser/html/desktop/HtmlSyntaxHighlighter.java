02a7a766f90ae4acd17d86e0177aa61cad630a12
Francisco Sokol <chico.sokol@gmail.com>
2012-06-25 18:52:09 -0300

    cleaning pygments command line options

--- src/main/java/br/com/caelum/tubaina/parser/html/desktop/HtmlSyntaxHighlighter.java
+++ src/main/java/br/com/caelum/tubaina/parser/html/desktop/HtmlSyntaxHighlighter.java
@@ -7,14 +7,14 @@ public class HtmlSyntaxHighlighter {
     private final CommandExecutor commandExecutor;
 
     public HtmlSyntaxHighlighter(CommandExecutor commandExecutor) {
-        this.commandExecutor = commandExecutor;
+        this.commandExecutor = commandExecutor; 
     }
 
     public String highlight(String code, String language, boolean numbered) {
         String options = "";
         if (numbered)
             options = "-P lineos=inline ";
-        String command = "pygmentize -O encoding=ISO-8859-1 -O outencoding=UTF-8 " + options + "-f html -l " + language;
+        String command = "pygmentize -O encoding=ISO-8859-1,outencoding=UTF-8 " + options + "-f html -l " + language;
         return commandExecutor.execute(command, code);
     }
 
