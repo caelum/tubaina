2fb721f22988abeeec8c55460f43acdf2585c898
Francisco Sokol <chico.sokol@gmail.com>
2012-07-06 18:59:36 -0300

    using hr again :(

--- src/main/java/br/com/caelum/tubaina/parser/html/kindle/BoxTag.java
+++ src/main/java/br/com/caelum/tubaina/parser/html/kindle/BoxTag.java
@@ -4,10 +4,10 @@ import br.com.caelum.tubaina.parser.Tag;
 
 public class BoxTag implements Tag {
 
-    static final String END = "</tr></td></table>";
-    static final String TITLE_END = "</b><br />\n";
+    static final String BEGIN = "<hr /><td>";
+    static final String END = "<hr />";
     static final String TITLE_BEGIN = "<b>";
-    static final String BEGIN = "<table><tr><td>";
+    static final String TITLE_END = "</b>\n";
 
     public String parse(String content, String title) {
         return BEGIN + TITLE_BEGIN + title.trim() + TITLE_END + content.trim()
