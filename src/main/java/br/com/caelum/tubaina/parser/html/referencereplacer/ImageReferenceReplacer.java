e28cc815505d4bf52c93ca62d87aab0e2d38e1a0
Francisco Sokol <chico.sokol@gmail.com>
2012-07-10 16:51:59 -0300

    checking null title while searching text to replace image reference

--- src/main/java/br/com/caelum/tubaina/parser/html/referencereplacer/ImageReferenceReplacer.java
+++ src/main/java/br/com/caelum/tubaina/parser/html/referencereplacer/ImageReferenceReplacer.java
@@ -4,18 +4,27 @@ import java.util.List;
 
 import net.htmlparser.jericho.Element;
 
+import org.apache.log4j.Logger;
+
 public class ImageReferenceReplacer extends AbstractReferenceReplacer {
 
+    private final Logger LOG = Logger.getLogger(ImageReferenceReplacer.class);
+    
     @Override
     protected String extractTextToReplaceReference(Element containerDiv, Element label) {
         String text = "*";
         if (label.getName().equals("img")) {
 
             Element title = containerDiv.getFirstElementByClass("referenceableTitle");
-            while (title.getStartTag().getName() != "h2") {
+            while (title != null && title.getStartTag().getName() != "h2") {
                 containerDiv = containerDiv.getParentElement();
                 title = containerDiv.getFirstElementByClass("referenceableTitle");
             }
+            if (title == null) {
+                LOG.warn("Could not resolve label: " + label.getAttributeValue("id"));
+                return "*";
+            }
+            
             List<Element> imgs = containerDiv.getAllElements("img");
             int imagePosition = imgs.indexOf(label) + 1;
             String chapterAndSectionNumber = title.getTextExtractor().toString().split("-")[0]
