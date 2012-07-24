44c873ce99c30c98a292bc92ce5b8f144d22f513
Caio Incau & Chico Sokol <chico.sokol@gmail.com>
2012-07-24 17:29:01 -0300

    should replace double quote for latex fancy quotes of text preceding '!'

--- src/test/java/br/com/caelum/tubaina/parser/RegexTagTest.java
+++ src/test/java/br/com/caelum/tubaina/parser/RegexTagTest.java
@@ -5,19 +5,41 @@ import static org.junit.Assert.assertEquals;
 import java.io.IOException;
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class RegexTagTest {
 
-    @Test
-    public void test() throws IOException {
-        List<Tag> regexTags = new RegexConfigurator().read("/regex.properties", "/latex.properties");
-        String text = "[ref-label lala_lala]";
+    private List<Tag> regexTags;
+
+    @Before
+    public void setUp() throws IOException {
+        regexTags = new RegexConfigurator().read("/regex.properties", "/latex.properties");
+    }
+
+    private String parseWithRegexps(String text) {
         for (Tag tag : regexTags) {
             text = tag.parse(text, "");
         }
-        assertEquals("\\ref{lala_lala}", text);
+        return text;
+    }
+
+    @Test
+    public void shouldReplaceFancyQuotes() throws Exception {
+        String text = "blablabla \"quoted\"";
+        text = parseWithRegexps(text);
+        assertEquals("blablabla ``quoted''", text);
+        
+        text = "\"e agora\"!";
+        text = parseWithRegexps(text);
+        assertEquals("``e agora''!", text);
+        
+        text = "\'e agora\'!";
+        text = parseWithRegexps(text);
+        assertEquals("`e agora'!", text);
         
     }
+    
+    
 
 }
