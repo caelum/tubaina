282e7399c77d48d10186b6686909949edecdef1f
Caio Incau & Chico Sokol <chico.sokol@gmail.com>
2012-07-24 17:14:02 -0300

    using table to separate boxes

--- src/main/java/br/com/caelum/tubaina/parser/html/kindle/BoxTag.java
+++ src/main/java/br/com/caelum/tubaina/parser/html/kindle/BoxTag.java
@@ -4,14 +4,14 @@ import br.com.caelum.tubaina.parser.Tag;
 
 public class BoxTag implements Tag {
 
-    static final String BEGIN = "<hr />";
-    static final String END = "<hr />";
+    static final String BEGIN = "<table cellspacing=\"5\" width=\"100%\" border=\"1\" class=\"box-table\"><colgroup>" +
+            "<col width=\"550\"/></colgroup><tbody><tr><td/></tr></table>";
+    static final String END = BEGIN;
     static final String TITLE_BEGIN = "<b>";
     static final String TITLE_END = "</b>\n";
-
+    
     public String parse(String content, String title) {
-        return BEGIN + TITLE_BEGIN + title.trim() + TITLE_END + content.trim()
-                + END;
+        return BEGIN + TITLE_BEGIN + title.trim() + TITLE_END + content.trim() + END;
     }
 
 }
