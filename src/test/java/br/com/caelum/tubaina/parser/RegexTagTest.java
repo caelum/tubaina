a21161b0bd866d2db05317c5c20537e817ac1f4f
Chico Sokol <chico.sokol@gmail.com>
2012-08-21 12:48:42 -0300

    fixing link issues

--- src/test/java/br/com/caelum/tubaina/parser/RegexTagTest.java
+++ src/test/java/br/com/caelum/tubaina/parser/RegexTagTest.java
@@ -37,9 +37,36 @@ public class RegexTagTest {
         text = "\'e agora\'!";
         text = parseWithRegexps(text);
         assertEquals("`e agora'!", text);
-        
     }
     
+    @Test
+    public void shouldReplaceCommonUrlForLink() throws Exception {
+        String text = "some text and a link: http://caelum.com.br more text";
+        text = parseWithRegexps(text);
+        assertEquals("some text and a link: \\link{http://caelum.com.br} more text", text);
+        
+        text = "some text and a https link: https://caelum.com.br more text";
+        text = parseWithRegexps(text);
+        assertEquals("some text and a https link: \\link{https://caelum.com.br} more text", text);
+    }
     
-
+    @Test
+    public void shouldNotConsiderPunctuationSymbolsAtTheEndOfUrlForLink() throws Exception {
+        testHttpAndHttps("This is a link ", "caelum.com.br", ".");
+        testHttpAndHttps("This is a link ", "caelum.com.br", ";");
+        testHttpAndHttps("This is a link ", "caelum.com.br", ":");
+        testHttpAndHttps("This is a link ", "caelum.com.br", ";");
+    }
+    
+    private void testHttpAndHttps(String textBefore, String link, String textAfter) {
+        String http = textBefore + "http://" +  link + textAfter;
+        String text = parseWithRegexps(http);
+        assertEquals(textBefore + "\\link{http://" + link + "}" + textAfter, text);
+        
+        String https = textBefore + "https://" +  link + textAfter;
+        text = parseWithRegexps(https);
+        assertEquals(textBefore + "\\link{https://" + link + "}" + textAfter, text);
+        
+        
+    }
 }
