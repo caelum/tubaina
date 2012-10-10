31a12243ceff5c99d2833ec5c78b3e36720cda26
Chico Sokol <chico.sokol@gmail.com>
2012-10-10 18:21:32 -0300

    no reason to use iconv anymore

--- etc/latex.sh
+++ etc/latex.sh
@@ -3,16 +3,12 @@ echo Gerando latex...
 
 HAS_ANSWER=`ls "$(dirname $1)" | grep answer.tex -c`
 if [ "$HAS_ANSWER" -eq "0" ]; then
-    touch "$(dirname $1)"/answer-utf.tex;
+    touch "$(dirname $1)"/answer.tex;
 else
-	mv answer.tex answer-utf.tex
+	mv answer.tex answer.tex
 fi
 
 cp -R ~/.tubaina/pythonenv .
-
-mv "$1" book-utf.tex
-iconv --from-code=UTF-8 --to-code=ISO_8859-1 book-utf.tex > "$1"
-iconv --from-code=UTF-8 --to-code=ISO_8859-1 answer-utf.tex > answer.tex
 
 echo "s\n" | pdflatex -shell-escape book.tex > /dev/null
 bibtex book
