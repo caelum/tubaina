55e9fa4ff7874fbad16a2b47954bb3767d9617e5
Chico Sokol <chico.sokol@gmail.com>
2012-10-10 18:34:35 -0300

    removing useless line

--- etc/latex.sh
+++ etc/latex.sh
@@ -4,8 +4,6 @@ echo Gerando latex...
 HAS_ANSWER=`ls "$(dirname $1)" | grep answer.tex -c`
 if [ "$HAS_ANSWER" -eq "0" ]; then
     touch "$(dirname $1)"/answer.tex;
-else
-	mv answer.tex answer.tex
 fi
 
 cp -R ~/.tubaina/pythonenv .
