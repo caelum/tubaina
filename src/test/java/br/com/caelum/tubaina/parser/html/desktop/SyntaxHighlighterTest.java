75ff9cf40ac17332d9e791c2e578832e473348bd
Sérgio Lopes <sergio.lopes@caelum.com.br>
2012-10-22 17:08:08 -0200

    Revert "forcing utf-8 as pygments input"
    This reverts commit fad119daac4561add61224afb22ffdf62e138001.

--- src/test/java/br/com/caelum/tubaina/parser/html/desktop/SyntaxHighlighterTest.java
+++ src/test/java/br/com/caelum/tubaina/parser/html/desktop/SyntaxHighlighterTest.java
@@ -18,7 +18,6 @@ public class SyntaxHighlighterTest {
     private CommandExecutor executor;
     private SyntaxHighlighter highlighter;
     private CodeCache codeCache;
-    private String encoding;
 
     @Before
     public void setUp() {
@@ -26,13 +25,13 @@ public class SyntaxHighlighterTest {
         this.executor = mock(CommandExecutor.class);
         this.codeCache = mock(CodeCache.class);
         this.highlighter = new SyntaxHighlighter(executor, SyntaxHighlighter.HTML_OUTPUT, false, codeCache);
-        this.encoding = "UTF-8";
     }
 
     @Test
     public void shouldCallPygmentsWithJavaLexer() throws Exception {
         String code = "public class Foo {\n" + "public int Bar(){\n" + "return 0;\n" + "}\n" + "}";
         highlighter.highlight(code, "java", false);
+        String encoding = System.getProperty("file.encoding");
         List<String> arguments = Arrays.asList("pygmentize", "-O", "encoding=" + encoding
                 + ",outencoding=UTF-8", "-f", "html", "-l", "java");
         verify(executor).execute(eq(arguments), eq(sampleCode));
@@ -41,6 +40,7 @@ public class SyntaxHighlighterTest {
     @Test
     public void shouldCallPygmentsWithNumberedLinesOption() throws Exception {
         highlighter.highlight(sampleCode, "java", true);
+        String encoding = System.getProperty("file.encoding");
         List<String> arguments = Arrays.asList("pygmentize", "-O", "encoding=" + encoding
                 + ",outencoding=UTF-8,linenos=inline", "-f", "html", "-l", "java");
         verify(executor).execute(eq(arguments), eq(sampleCode));
@@ -48,8 +48,12 @@ public class SyntaxHighlighterTest {
 
     @Test
     public void shouldCallPygmentsWithHlLines() throws Exception {
+
         List<Integer> lines = Arrays.asList(1, 2, 5);
+
         highlighter.highlight(sampleCode, "java", false, lines);
+
+        String encoding = System.getProperty("file.encoding");
 
         List<String> arguments = Arrays.asList("pygmentize", "-O", "encoding=" + encoding
                 + ",outencoding=UTF-8,hl_lines=1 2 5", "-f", "html", "-l", "java");
@@ -61,6 +65,7 @@ public class SyntaxHighlighterTest {
     public void shouldCallPygmentsWithLatexOutput() throws Exception {
         this.highlighter = new SyntaxHighlighter(executor, SyntaxHighlighter.LATEX_OUTPUT, false, codeCache);
         highlighter.highlight(sampleCode, "java", false);
+        String encoding = System.getProperty("file.encoding");
         List<String> arguments = Arrays.asList("pygmentize", "-O", "encoding=" + encoding
                 + ",outencoding=UTF-8", "-P", "verboptions=numbersep=5pt", "-f", "latex", "-l", "java");
         verify(executor).execute(eq(arguments), eq(sampleCode));
