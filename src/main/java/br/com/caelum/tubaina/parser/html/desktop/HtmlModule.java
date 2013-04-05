e1161b861394892dd9f67d37558443006555235c
Cecilia Fernandes <ceci.vidageek@gmail.com>
2013-04-05 15:33:37 -0300

    [Adding Guice] TodoTagTest and Manifest on build.gradle.

--- src/main/java/br/com/caelum/tubaina/parser/html/desktop/HtmlModule.java
+++ src/main/java/br/com/caelum/tubaina/parser/html/desktop/HtmlModule.java
@@ -15,6 +15,7 @@ import br.com.caelum.tubaina.chunk.QuestionChunk;
 import br.com.caelum.tubaina.chunk.TableChunk;
 import br.com.caelum.tubaina.chunk.TableColumnChunk;
 import br.com.caelum.tubaina.chunk.TableRowChunk;
+import br.com.caelum.tubaina.chunk.TodoChunk;
 import br.com.caelum.tubaina.parser.IntroductionTag;
 import br.com.caelum.tubaina.parser.NullAnswerTag;
 import br.com.caelum.tubaina.parser.NullNoteTag;
@@ -56,6 +57,7 @@ public class HtmlModule extends TubainaModule {
 		bind(new TypeLiteral<Tag<TableColumnChunk>>() {}).to(TableColumnTag.class);
 		bind(new TypeLiteral<Tag<TableRowChunk>>() {}).to(TableRowTag.class);
 		bind(new TypeLiteral<Tag<TableChunk>>() {}).to(TableTag.class);
+		bind(new TypeLiteral<Tag<TodoChunk>>() {}).to(TodoTag.class);
 		
 		bind(Sanitizer.class).to(HtmlSanitizer.class);
 	}
---
+++ src/test/java/br/com/caelum/tubaina/parser/html/kindle/TodoTagTest.java
@@ -0,0 +1,16 @@
+package br.com.caelum.tubaina.parser.html.kindle;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import br.com.caelum.tubaina.chunk.TodoChunk;
+
+public class TodoTagTest extends AbstractTagTest {
+
+	@Test
+	public void todoContentIsEmpty() throws Exception {
+		TodoChunk chunk = new TodoChunk("Something I have to do later");
+		String content = getContent(chunk);
+		Assert.assertTrue(content.isEmpty());
+	}
+}
