19aeb58f529960f1bfc7932c0237ad2d3e2ecbe6
Cecilia Fernandes <ceci.vidageek@gmail.com>
2013-04-24 19:00:29 -0300

    Differentiating numbered code from unnumbered on code cache

--- src/test/java/br/com/caelum/tubaina/parser/html/desktop/CodeTagTest.java
+++ src/test/java/br/com/caelum/tubaina/parser/html/desktop/CodeTagTest.java
@@ -7,24 +7,37 @@ import br.com.caelum.tubaina.chunk.CodeChunk;
 
 public class CodeTagTest extends AbstractTagTest {
 
-    @Test
-    public void shouldCallHtmlCodeTag() {
-        String code = "public static void main(String[] args) {\n" +
-                "    String name = \"Gabriel\";\n" +
-                "    System.out.println(\"Hello, \" + name);\n" +
-                "}";
-        CodeChunk chunk = new CodeChunk(code, "");
+	@Test
+	public void notNumberedGenericCode() {
+		String code = "public static void main(String[] args) {\n" + "    String name = \"Gabriel\";\n"
+				+ "    System.out.println(\"Hello, \" + name);\n" + "}";
+		CodeChunk chunk = new CodeChunk(code, "");
 		String result = getContent(chunk);
-		Assert.assertEquals("<pre ><div class=\"highlight\">" +
-								"<pre><span class=\"lineno\">1</span> " +
-								"public static void main(String[] args) {" +
-								"\n<span class=\"lineno\">2</span> " +
-								"    String name = &quot;Gabriel&quot;;" +
-								"\n<span class=\"lineno\">3</span> " +
-								"    System.out.println(&quot;Hello, &quot; + name);" +
-								"\n<span class=\"lineno\">4</span>" +
-								" }" +
-								"\n</pre>" +
+		Assert.assertEquals("<pre ><div class=\"highlight\">" + 
+							"<pre>public static void main(String[] args) {" + 
+							"\n    String name = &quot;Gabriel&quot;;" + 
+							"\n    System.out.println(&quot;Hello, &quot; + name);" + 
+							"\n}" + 
+							"\n</pre>" + 
+							"</div></pre>", result);
+	}
+
+	@Test
+	public void numberedGenericCode() {
+		String code = "public static void main(String[] args) {\n" + "    String name = \"Gabriel\";\n"
+				+ "    System.out.println(\"Hello, \" + name);\n" + "}";
+		CodeChunk chunk = new CodeChunk(code, "#");
+		String result = getContent(chunk);
+		Assert.assertEquals("<pre ><div class=\"highlight\">" + 
+							"<pre><span class=\"lineno\">1</span> " + 
+							"public static void main(String[] args) {" + 
+							"\n<span class=\"lineno\">2</span> " + 
+							"    String name = &quot;Gabriel&quot;;" + 
+							"\n<span class=\"lineno\">3</span> " + 
+							"    System.out.println(&quot;Hello, &quot; + name);" + 
+							"\n<span class=\"lineno\">4</span>" + 
+							" }" + 
+							"\n</pre>" + 
 							"</div></pre>", result);
 	}
 }
