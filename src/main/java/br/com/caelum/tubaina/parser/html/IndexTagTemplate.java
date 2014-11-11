ab285735b143edc594911e56246fcc90f15e9f96
Francisco Sokol <chico.sokol@gmail.com>
2014-11-11 14:15:10 -0200

    dropping support for index tag in htmlk

--- src/main/java/br/com/caelum/tubaina/parser/html/IndexTagTemplate.java
+++ src/main/java/br/com/caelum/tubaina/parser/html/IndexTagTemplate.java
@@ -3,10 +3,12 @@ package br.com.caelum.tubaina.parser.html;
 import br.com.caelum.tubaina.chunk.IndexChunk;
 import br.com.caelum.tubaina.parser.Tag;
 import br.com.caelum.tubaina.util.Sanitizer;
+import org.apache.log4j.Logger;
 
 public class IndexTagTemplate implements Tag<IndexChunk> {
 
 	private final Sanitizer sanitizer;
+	private static final Logger LOG = Logger.getLogger(IndexTagTemplate.class);
 
 	public IndexTagTemplate(Sanitizer sanitizer) {
 		this.sanitizer = sanitizer;
@@ -15,7 +17,8 @@ public class IndexTagTemplate implements Tag<IndexChunk> {
 	@Override
 	public String parse(IndexChunk chunk) {
 		String index = sanitizer.sanitize(chunk.getName());
-		return "\n<a id=\"" + index + "\"></a>\n";
+		LOG.warn("HTML format doesn't support [index] tag");
+		return "";
 	}
 
 }
