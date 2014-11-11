6ebc644cb3538c46378e205f195c3d6de65dafbb
Francisco Sokol <chico.sokol@gmail.com>
2014-11-11 10:36:48 -0200

    cleaning code cache before tests

--- src/test/java/br/com/caelum/tubaina/parser/html/desktop/AbstractTagTest.java
+++ src/test/java/br/com/caelum/tubaina/parser/html/desktop/AbstractTagTest.java
@@ -1,12 +1,25 @@
 package br.com.caelum.tubaina.parser.html.desktop;
 
+import java.io.File;
 import java.util.Arrays;
 import java.util.List;
 
 import br.com.caelum.tubaina.Chunk;
 import br.com.caelum.tubaina.chunk.MockedChunk;
+import org.junit.Before;
 
 public class AbstractTagTest {
+
+	@Before
+	public final void cleanCodeCache() {
+		String dir = System.getProperty("java.io.tmpdir");
+		File tempdir = new File(dir);
+		File[] files = tempdir.listFiles();
+		for (File file : files) {
+			file.delete();
+		}
+	}
+
 	protected String getContent(Chunk chunk) {
 		new HtmlModule().inject(chunk);
 		return chunk.asString();
