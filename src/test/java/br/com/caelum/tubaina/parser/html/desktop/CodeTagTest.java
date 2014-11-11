2c3b14b6c24517eb04a5c153a58f6e5fef4bea57
Francisco Sokol <chico.sokol@gmail.com>
2014-11-11 11:31:01 -0200

    using div instead of pre to create a valid xhtml

--- src/test/java/br/com/caelum/tubaina/parser/html/desktop/CodeTagTest.java
+++ src/test/java/br/com/caelum/tubaina/parser/html/desktop/CodeTagTest.java
@@ -1,9 +1,14 @@
 package br.com.caelum.tubaina.parser.html.desktop;
 
+import br.com.caelum.tubaina.parser.html.HtmlAndKindleCodeTag;
 import org.junit.Assert;
 import org.junit.Test;
 
 import br.com.caelum.tubaina.chunk.CodeChunk;
+
+import static br.com.caelum.tubaina.parser.html.HtmlAndKindleCodeTag.BEGIN_END;
+import static br.com.caelum.tubaina.parser.html.HtmlAndKindleCodeTag.BEGIN_START;
+import static br.com.caelum.tubaina.parser.html.HtmlAndKindleCodeTag.END;
 
 public class CodeTagTest extends AbstractTagTest {
 
@@ -13,13 +18,13 @@ public class CodeTagTest extends AbstractTagTest {
 				+ "    System.out.println(\"Hello, \" + name);\n" + "}";
 		CodeChunk chunk = new CodeChunk(code, "");
 		String result = getContent(chunk);
-		Assert.assertEquals("<pre ><div class=\"highlight\">" + 
+		Assert.assertEquals(BEGIN_START + BEGIN_END + "<div class=\"highlight\">" +
 							"<pre>public static void main(String[] args) {" + 
 							"\n    String name = &quot;Gabriel&quot;;" + 
 							"\n    System.out.println(&quot;Hello, &quot; + name);" + 
 							"\n}" + 
 							"\n</pre>" + 
-							"</div></pre>", result);
+							"</div>" + END, result);
 	}
 
 	@Test
@@ -28,7 +33,7 @@ public class CodeTagTest extends AbstractTagTest {
 				+ "    System.out.println(\"Hello, \" + name);\n" + "}";
 		CodeChunk chunk = new CodeChunk(code, "#");
 		String result = getContent(chunk);
-		Assert.assertEquals("<pre ><div class=\"highlight\">" + 
+		Assert.assertEquals(BEGIN_START + BEGIN_END + "<div class=\"highlight\">" +
 							"<pre><span class=\"lineno\">1</span> " + 
 							"public static void main(String[] args) {" + 
 							"\n<span class=\"lineno\">2</span> " + 
@@ -38,6 +43,6 @@ public class CodeTagTest extends AbstractTagTest {
 							"\n<span class=\"lineno\">4</span>" + 
 							" }" + 
 							"\n</pre>" + 
-							"</div></pre>", result);
+							"</div>" + END, result);
 	}
 }
